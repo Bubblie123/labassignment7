3c15efaf3a76197016b5a2bc6a939467158a3c3c
Bubblie123 <rohan.suresh712@gmail.com>
2023-03-22 16:41:11 -0400

    Changed Swap Code

--- bubbleSortIndexCounter.c
+++ bubbleSortIndexCounter.c
@@ -1,23 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int bubbleSort(int arr[], int n);
+void bubbleSort(int arr[], int n);
 void printArray(int arr[], int n);
 
 int main() {
     int arr[] = {97,16,45,63,13,22,7,58,72};
     printf("Original Array:\n");
     printArray(arr, 9);
-    printf("Number of Swaps Required: %d\n", bubbleSort(arr, 9));
-    printf("Bubble Sorted Array:\n");
-    printArray(arr, 9);
-
+    bubbleSort(arr, 9);
     return 0;
 }
 
-int bubbleSort(int arr[], int n) {
-    int swapCounter = 0;
+void bubbleSort(int arr[], int n) {
     for (int i = 0; i < n - 1; ++i) {
+        int swapCounter = 0;
         for (int j = 0; j < n - i - 1; ++j) {
             if (arr[j] > arr[j + 1]) {
                 int temp = arr[j];
@@ -26,6 +23,7 @@ int bubbleSort(int arr[], int n) {
                 ++swapCounter;
             }
         }
+        printf("Number of Swaps Required For This Iteration: %d\n", swapCounter);
     }
 }
 
